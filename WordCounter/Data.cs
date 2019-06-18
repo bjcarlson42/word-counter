789b5f7b044e12588c14a7a63b4b9759a75e5e41
Ben Carlson <bjcarlson42@outlook.com>
2019-06-18 14:16:27 -0400

    Added the list data strucuture.

---
+++ WordCounter/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCounter
+{
+    class Data
+    {
+        public string word;
+        public int count;
+    }
+}
